c6549a8fdfd61b5bf94e4647ecca229cab1638f5
NISHAJMORE <120033928+NISHAJMORE@users.noreply.github.com>
2024-02-04 15:15:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+var  first_name ="Nishaj";
+console.log(first_name);
+
+var last_name ="More";
+console.log(last_name);
+
+
+var a=10;
+var b=5;
+
+var add = a+b ;
+console.log(add);
+ 
+var full_name = first_name.concat(last_name) ;
+console.log(full_name);
+
+
+var substract =a-b;
+console.log(substract);
+  
+var capital_name = "NISHAJ" ;
+console.log(capital_name);
+ 
+var making_lower_case = capital_name.toLowerCase();
+console.log(making_lower_case);
+
+
+var small_name = "nishu" ;
+console.log(small_name);
+
+var making_capital =small_name.toUpperCase();
+console.log(making_capital);
+
+
+
+
+
+
+
+
+
